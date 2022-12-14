70df429ba5eff06e29b5378f6f630bdea61bab42
dzmitry <dradzevich@ziyotek.net>
2022-12-14 18:29:12 -0500

    1

--- files/MODULES/FILES/jenkins-bootstrap-script.sh
+++ files/MODULES/FILES/jenkins-bootstrap-script.sh
@@ -31,9 +31,9 @@
 </head>
 <body>
   <div align="center">
-    <h1>Congratulations-DIMON-KRASAVA!!! 2022 DevOps Class</h1>
+    <h1>Congratulations-TEAM2 2022 DevOps Class</h1>
     <h2>This application was deployed using Jenkins.</h2>
-    <h3>Next Step is starting a new career.</h3>
+    <h3>Next Step go to drink and start a new career.</h3>
     <p>For next steps, read the <a href="http://aws.amazon.com/documentation/codedeploy">AWS CodeDeploy Documentation</a>.</p>
   </div>
 </body>
